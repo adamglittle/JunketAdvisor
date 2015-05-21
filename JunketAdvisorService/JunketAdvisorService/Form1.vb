3cb0abb73f64a7691cf084564ac6bf1fed7ea2e5
adamglittle <adam.g.little@gmail.com>
2015-05-21 20:51:41 +1000

    return members from db as jsonp

--- JunketAdvisorService/JunketAdvisorService/Form1.vb
+++ JunketAdvisorService/JunketAdvisorService/Form1.vb
@@ -1,3 +1,119 @@
-﻿Public Class Form1
+﻿Imports System.ServiceModel
+Imports System.ServiceModel.Web
+Imports System.ServiceModel.Description
+Imports System.Text
+Imports System.Threading
+Imports System.Security.Principal
+Imports System.Net
+Imports System.ServiceModel.Security
+Imports System.IdentityModel.Selectors
+Imports System.Web.Script.Serialization
+
+
+
+Public Class Form1
+    Private fMembers As New Members
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+
+            Dim host As WebServiceHost = New WebServiceHost(GetType(Service), New Uri("http://0.0.0.0/"))
+
+            Dim binding As New WebHttpBinding()
+            binding.Security.Mode = WebHttpSecurityMode.TransportCredentialOnly
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic
+            binding.Security.Transport.Realm = "this realm"
+
+
+
+            Dim ep As Description.ServiceEndpoint = host.AddServiceEndpoint(GetType(IService), binding, "http://localhost:8005/")
+
+            host.Credentials.UserNameAuthentication.UserNamePasswordValidationMode = UserNamePasswordValidationMode.Custom
+            host.Credentials.UserNameAuthentication.CustomUserNamePasswordValidator = New CustomUserNamePasswordValidator()
+
+            host.Open()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    Public Class CustomUserNamePasswordValidator
+        Inherits UserNamePasswordValidator
+        Public Overrides Sub Validate(userName As String, password As String)
+            'Your logic to validate username/password
+            If userName <> password Then
+                Throw New FaultException("access denied")
+            End If
+        End Sub
+    End Class
+
+End Class
+
+<ServiceContract()> _
+Public Interface IService
+    '<OperationContract()> _
+    '<WebGet()> _
+    'Function EchoWithGet(ByVal s As String) As String
+
+    <OperationContract()> _
+     <WebGet(responseformat:=WebMessageFormat.Json)> _
+    Function GetMember(ByVal s As String, ByVal callback As String) As IO.Stream
+
+    <OperationContract()> _
+    <WebInvoke()> _
+    Function EchoWithPost(ByVal s As String) As String
+
+    <OperationContract()> _
+    <WebGet(responseformat:=WebMessageFormat.Json)> _
+    Function GetCityImage(ByVal city As String, ByVal callback As String) As IO.Stream
+
+End Interface
+
+Public Class Service
+    Implements IService
+    'Public Function EchoWithGet(ByVal s As String) As String Implements IService.EchoWithGet
+    '    Return "You said " + s
+    'End Function
+
+    Public Function GetMember(ByVal s As String, ByVal callback As String) As IO.Stream Implements IService.GetMember
+        Dim strAuthHeader As String = WebOperationContext.Current.IncomingRequest.Headers.Get("Authorization")
+
+
+
+        If strAuthHeader Is Nothing Then
+            WebOperationContext.Current.OutgoingResponse.StatusCode = Net.HttpStatusCode.Unauthorized
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text"
+            Return New IO.MemoryStream(Encoding.UTF8.GetBytes("Unauthorised"))
+        End If
+
+        Dim fMember As New Members
+        Dim thisMember As Member = fMember.GetMember(s)
+
+        Dim ser As New JavaScriptSerializer
+
+        Dim dict = ser.Serialize(thisMember)
+
+
+        WebOperationContext.Current.OutgoingResponse.ContentType = "application/javascript"
+        Dim ms As New IO.MemoryStream(Encoding.UTF8.GetBytes(callback & "(" + dict.ToString + ")"))
+        Return ms
+    End Function
+
+    Public Function EchoWithPost(ByVal s As String) As String Implements IService.EchoWithPost
+        Return "You said " + s
+    End Function
+
+    Public Function GetCityImage(ByVal city As String, ByVal callback As String) As IO.Stream Implements IService.GetCityImage
+        Dim fs As IO.FileStream = IO.File.OpenRead("..\..\images\" & city & ".jpg")
+        Dim imageBytes(fs.Length - 1) As Byte
+        fs.Read(imageBytes, 0, fs.Length - 1)
+        Dim returnString As String = callback & "(""" & Convert.ToBase64String(imageBytes) & """)"
+        WebOperationContext.Current.OutgoingResponse.ContentType = "application/javascript"
+        Return New IO.MemoryStream(Encoding.UTF8.GetBytes(returnString))
+    End Function
 
 End Class
--- JunketAdvisorService/JunketAdvisorService/Members.vb
+++ JunketAdvisorService/JunketAdvisorService/Members.vb
@@ -1,4 +1,6 @@
-﻿Public Class Members
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+Public Class Members
     Public Sub AddMember(thisMember As Member)
 
     End Sub
@@ -8,7 +10,90 @@
 
         Return Nothing
     End Function
+    Public Function GetMember(email As String) As Member
+        Dim thisMember As New Member
+
+        Dim conn As New SqlConnection(GetConnectionStringByName("JunketAdvisorService.My.MySettings.JaDbConnectionString"))
+
+        ' if this member is not in the db then add it
+        Dim cmd As New SqlCommand("select * from members where emailAddress = @email", conn)
+        cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = email
+
+        Dim da As New SqlDataAdapter(cmd)
+        Dim dt As New DataTable
+        Try
+            da.Fill(dt)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+        For Each dr As DataRow In dt.Rows
+            thisMember.id = dr("id")
+            thisMember.emailAddress = CType(dr("emailAddress"), String).Trim
+            thisMember.firstName = CType(dr("firstName"), String).Trim
+            thisMember.lastName = CType(dr("lastName"), String).Trim
+            Return thisMember
+        Next
+
+
+        Return Nothing
+    End Function
+
+    Private Sub PersistMember(thisMember As Member)
+        Dim conn As New SqlConnection(GetConnectionStringByName("JunketAdvisorService.My.MySettings.JaDbConnectionString"))
+        Dim da As New SqlDataAdapter("select * from members", conn)
+
+        ' if this member is not in the db then add it
+        Dim cmd As New SqlCommand("select * from members where emailAddress = @email", conn)
+        cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = thisMember.emailAddress
+
+        If cmd.ExecuteReader.HasRows Then
+
+        Else
+            Dim query As String = String.Empty
+            query &= "INSERT INTO member (emailAddress, firstName, lastName, "
+            query &= "                     role)  "
+            query &= "VALUES (@emailAddress,@firstName, @lastName, @role)"
+
+            Using comm As New SqlCommand()
+                With comm
+                    .Connection = conn
+                    .CommandType = CommandType.Text
+                    .CommandText = query
+                    .Parameters.AddWithValue("@emailAddress", thisMember.emailAddress)
+                    .Parameters.AddWithValue("@firstName", thisMember.firstName)
+                    .Parameters.AddWithValue("@lastName", thisMember.lastName)
+                    .Parameters.AddWithValue("@role", thisMember.role)
+                End With
+                Try
+                    conn.Open()
+                    comm.ExecuteNonQuery()
+                Catch ex As SqlException
+                    MessageBox.Show(ex.Message.ToString(), "Error Message")
+                End Try
+            End Using
+
+        End If
+    End Sub
+    Private Shared Function GetConnectionStringByName( _
+    ByVal name As String) As String
+
+        ' Assume failure 
+        Dim returnValue As String = Nothing
+
+        ' Look for the name in the connectionStrings section. 
+        Dim settings As ConnectionStringSettings = _
+           ConfigurationManager.ConnectionStrings(name)
+
+        ' If found, return the connection string. 
+        If Not settings Is Nothing Then
+            returnValue = settings.ConnectionString
+        End If
+
+        Return returnValue
+    End Function
 End Class
+<Serializable> _
 Public Class Member
     Private _id As Integer
     Private _firstName As String
@@ -16,10 +101,13 @@ Public Class Member
     Private _emailAddress As String
     Private _role As String
 
-    Public ReadOnly Property id As Integer
+    Public Property id As Integer
         Get
             Return _id
         End Get
+        Set(value As Integer)
+            _id = value
+        End Set
     End Property
     Public Property firstName As String
         Get
